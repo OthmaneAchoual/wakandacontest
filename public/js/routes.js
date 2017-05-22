8ca03bfccad027bdb43a98e504ee9c5268a9e23d
Othmane Achoual <othmaneachoual@iMac-de-Othmane.local>
2017-05-22 15:55:24 +0000

    add a service, a controller and some templates

--- public/js/routes.js
+++ public/js/routes.js
@@ -2,5 +2,10 @@ angular.module('LibraryApp')
 	.config(['$routeProvider', function($routeProvider) {
 		$routeProvider.when('/', {
 			templateUrl: 'templates/main.html'
-		});
+		}).when('/login', {
+			templateUrl: 'templates/login.html'
+		}).when('/book/:id', {
+			templateUrl: 'templates/showbook.html',
+			controller: 'ShowBookController'
+		})
 	}]);
